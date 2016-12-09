d662cfacf3aef55a896f6760d75475142a407758
Burak Alparslan <Burak.Alparslan@monitise.com>
2016-12-10 01:34:22 +0300

    Added heroku configurations

---
+++ src/main/java/com/budofa/store/DatabaseConfig.java
@@ -0,0 +1,18 @@
+package com.budofa.store;
+
+import org.apache.tomcat.jdbc.pool.DataSource;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+@Configuration
+public class DatabaseConfig {
+
+    @Bean
+    @Primary
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSource dataSource() {
+        return new DataSource();
+    }
+}
