45529b0780bbf013c72c59710906dde4512861a9
Burak Alparslan <brk.alprsln@gmail.com>
2017-04-10 21:01:22 +0300

    Added user dto

--- src/main/java/com/budofa/store/controler/model/UserDTO.java
+++ src/main/java/com/budofa/store/controler/model/UserDTO.java
@@ -1,4 +1,136 @@
 package com.budofa.store.controler.model;
 
+import com.budofa.store.model.MemberShipType;
+
 public class UserDTO extends BaseDTO {
+
+    private String firstName;
+
+    private String middleName;
+
+    private String lastName;
+
+    private String userName;
+
+    private MemberShipType memberShipType;
+
+    private String password;
+
+    private String email;
+
+    private String mobileNo;
+
+    private AddressDTO address;
+
+    private String imageUrl;
+
+    private String identityNo;
+
+    private RoleDTO role;
+
+    private boolean enabled;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public MemberShipType getMemberShipType() {
+        return memberShipType;
+    }
+
+    public void setMemberShipType(MemberShipType memberShipType) {
+        this.memberShipType = memberShipType;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getMobileNo() {
+        return mobileNo;
+    }
+
+    public void setMobileNo(String mobileNo) {
+        this.mobileNo = mobileNo;
+    }
+
+    public AddressDTO getAddress() {
+        return address;
+    }
+
+    public void setAddress(AddressDTO address) {
+        this.address = address;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getIdentityNo() {
+        return identityNo;
+    }
+
+    public void setIdentityNo(String identityNo) {
+        this.identityNo = identityNo;
+    }
+
+    public RoleDTO getRole() {
+        return role;
+    }
+
+    public void setRole(RoleDTO role) {
+        this.role = role;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
 }
